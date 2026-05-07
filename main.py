5e8c23ae61477c4d616bfe00a7fa163bd03902e3
Khansa <aiengineer.quantristech@gmail.com>
2026-05-07 12:05:23 +0500

    add create all tables

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ import os
 
 app = FastAPI()
 
+from database import Base, engine
+Base.metadata.create_all(bind=engine)
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
